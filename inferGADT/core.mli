300268b7b1ceb2795f3933c8f09ffc21bc5105f8
KomaEc <31341411+KomaEc@users.noreply.github.com>
2018-05-13 09:21:55 +0800

    Delete core.mli

--- inferGADT/core.mli
+++
@@ -1,9 +0,0 @@
-open Format
-open Syntax
-val unify : (monoTy * monoTy) list -> substitution option
-val gather_subst : type_env -> term -> monoTy -> substitution option
-val infer : type_env -> term -> (int -> monoTy) option
-val gather_exp_ty_substitution :
-  type_env -> term -> monoTy -> (proof * substitution) option
-val niceInfer_exp : (type_env -> term -> monoTy -> (proof * substitution) option) ->
-  type_env -> term -> string
